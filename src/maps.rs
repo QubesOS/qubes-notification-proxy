7c4fbd67d03546372c2cbb8a8c99eba56e6180b0
Demi Marie Obenour <demi@invisiblethingslab.com>
2024-03-05 18:57:29 -0500

    Add missing file
    It should have been added long ago.

---
+++ src/maps.rs
@@ -0,0 +1,112 @@
+use core::num::NonZeroU32;
+
+#[derive(Copy, Clone)]
+#[repr(transparent)]
+pub struct HostId(NonZeroU32);
+#[repr(transparent)]
+#[derive(Copy, Clone)]
+pub struct GuestId(NonZeroU32);
+
+impl HostId {
+    pub fn new_less_safe(v: u32) -> Option<Self> {
+        NonZeroU32::new(v).map(Self)
+    }
+}
+
+impl GuestId {
+    pub fn new_less_safe(v: u32) -> Option<Self> {
+        NonZeroU32::new(v).map(Self)
+    }
+}
+
+impl From<GuestId> for u32 {
+    fn from(value: GuestId) -> Self {
+        value.0.into()
+    }
+}
+
+impl From<HostId> for u32 {
+    fn from(value: HostId) -> Self {
+        value.0.into()
+    }
+}
+
+pub(super) struct Maps {
+    guest_to_host_map: std::collections::BTreeMap<NonZeroU32, NonZeroU32>,
+    host_to_guest_map: std::collections::BTreeMap<NonZeroU32, NonZeroU32>,
+    last_id: NonZeroU32,
+}
+
+impl Default for Maps {
+    fn default() -> Self {
+        Self {
+            guest_to_host_map: Default::default(),
+            host_to_guest_map: Default::default(),
+            last_id: 1.try_into().expect("constant value"),
+        }
+    }
+}
+
+fn next(t: NonZeroU32) -> NonZeroU32 {
+    match t.into() {
+        u32::MAX => 1,
+        e => e + 1,
+    }
+    .try_into()
+    .expect("always produces nonzero value")
+}
+
+impl Maps {
+    pub(super) fn next_id(&mut self, id: HostId) -> GuestId {
+        self.last_id = next(self.last_id);
+        while self.guest_to_host_map.contains_key(&self.last_id) {
+            self.last_id = next(self.last_id);
+        }
+        let last_id = self.last_id;
+        eprintln!("Next ID is {}, mapping to host ID {}", last_id, id.0);
+        assert!(self
+            .guest_to_host_map
+            .insert(last_id, id.0.into())
+            .is_none());
+        assert!(
+            self.host_to_guest_map
+                .insert(id.0.into(), last_id)
+                .is_none(),
+            "notification daemon reused an ID without telling us"
+        );
+        GuestId(
+            last_id
+                .try_into()
+                .expect("last ID set to a nonzero value above"),
+        )
+    }
+
+    pub(super) fn lookup_guest_id(&self, id: GuestId) -> Option<HostId> {
+        self.guest_to_host_map.get(&id.0.into()).map(|&e| HostId(e))
+    }
+
+    pub(super) fn lookup_host_id(&self, id: HostId) -> Option<GuestId> {
+        self.host_to_guest_map
+            .get(&id.0.into())
+            .map(|&e| GuestId(e))
+    }
+
+    pub(super) fn remove_host_id(&mut self, id: HostId) -> Option<GuestId> {
+        let guest = self.host_to_guest_map.remove(&id.0.into());
+        match guest {
+            None => {
+                eprintln!("Trying to delete nonexistant ID {}", u32::from(id.0));
+                return None;
+            }
+            Some(g) => {
+                assert_eq!(self.guest_to_host_map.remove(&g.into()), id.0.into());
+                Some(GuestId(g))
+            }
+        }
+    }
+
+    pub(super) fn clear(&mut self) {
+        self.guest_to_host_map.clear();
+        self.host_to_guest_map.clear();
+    }
+}
